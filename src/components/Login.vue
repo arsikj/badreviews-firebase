cce716ca18a8b8229c6f8a2c5a64a49c82e5a7f3
arsikj <dejan_arsic@live.com>
2017-10-07 17:29:18 +0200

    initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="login">
+    <h3>Sign in</h3>
+    <input type="email" v-model="email" placeholder="Email"><br>
+    <input type="password" v-model="password" placeholder="Password"><br>
+    <button v-on:click="login">Connection</button>
+    <p>You don't have an account? You can <router-link to="/sign-up">create one</router-link></p>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+  name: 'login',
+  data: function(){
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login: function(){
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+        (user) => {
+          this.$router.replace('hello');
+        },
+        (err) => {
+          alert('Oops. ' + err.message);
+        }
+      );
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .login {
+    margin-top: 40px;
+  }
+  input {
+    margin: 10px 0;
+    width: 20%;
+    padding: 15px;
+  }
+  button {
+    margin-top: 20px;
+    cursor: pointer;
+  }
+  p {
+    margin-top: 40px;
+    font-size: 13px;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+  p a {
+
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import firebase from 'firebase'
+
+Vue.config.productionTip = false
+
+let app;
+let config = {
+    apiKey: "AIzaSyAvdHpLf9WAvbpzN72oYdFFWunxA03qk8M",
+    authDomain: "badreviews-1c0c4.firebaseapp.com",
+    databaseURL: "https://badreviews-1c0c4.firebaseio.com",
+    projectId: "badreviews-1c0c4",
+    storageBucket: "badreviews-1c0c4.appspot.com",
+    messagingSenderId: "481022068097"
+  };
+firebase.initializeApp(config);
+firebase.auth().onAuthStateChanged(function(user){
+  if(!app) {
+    app = new Vue({
+      el: '#app',
+      template: '<App/>',
+      components: {App},
+      router
+    })
+  }
+});
